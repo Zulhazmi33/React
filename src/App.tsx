8024a9b86f117ae21cf151dd605f485314c851ff
Raja Ahmad Zulhazmi <lazicdeveloper.hq@gmail.com>
2026-02-10 11:13:42 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const test = <button>hai</button>;
+  const image = 
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+  const card = 
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+  const read = 
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+  const title = <h1>Vite + React + {test}</h1>
+  return (
+    <>
+      {image}
+      {title}
+      {card}
+      {read}
+    </>
+  )
+}
+
+export default App
