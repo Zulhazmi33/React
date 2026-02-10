4112f7857157932797ba3699c56711d835430e1a
Raja Ahmad Zulhazmi <lazicdeveloper.hq@gmail.com>
2026-02-10 11:34:55 +0800

    create new files

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,30 @@ function App() {
 
   const test = <button>hai</button>;
   const image = 
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <div>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+    </div>
   const card = 
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-  const read = 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
       </p>
+    </div>
+  const read = 
+    <p className="read-the-docs">
+      Click on the Vite and React logos to learn more
+    </p>
   const title = <h1>Vite + React + {test}</h1>
+
+  
   return (
     <>
       {image}
