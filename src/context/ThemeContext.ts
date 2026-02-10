0a97aa1909fe870c094658ac6bbfbd71c226bea8
Raja Ahmad Zulhazmi <lazicdeveloper.hq@gmail.com>
2026-02-10 12:13:46 +0800

    sample

--- src/context/ThemeContext.ts
+++ src/context/ThemeContext.ts
@@ -1,4 +1,4 @@
-import type { ThemeContextType } from './themeProvider'
+import type { ThemeContextType } from './themeProvider';
 import { createContext } from 'react';
 
 export const ThemeContext = createContext<ThemeContextType>({
